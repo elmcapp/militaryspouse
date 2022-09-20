a82b4d6f313441082e05ed16e2858f113b32e7a6
elmcapp <ecammlivemidicontroller@gmail.com>
2022-09-20 19:36:21 -0400

    changed file sturcture

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,11 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
-import HomeFeaturedPost from "../components/HomeFeaturedPost";
+import HomeFeaturedPost from "./components/HomeFeaturedPost";
 
 function Home() {
   return (
     <div>
       <Box sx={{ margin: 5 }}>
-        {/* <Typography variant="h3" sx={{ fontWeight: "bold" }}>
-          Military Spouse Interstate License Recognition Options
-        </Typography> */}
-
         <HomeFeaturedPost />
       </Box>
     </div>
